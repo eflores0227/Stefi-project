15026d0458e7f39be7c37f157954ef3303021802
Estefania Flores <eflores0227@gmail.com>
2017-10-12 20:58:38 -0700

    day two of angular, before css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public query: string;
+  public queryString: string;
+  public postList;
+  title = 'my project';
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit() {
+  }
+
+  searchPosts() {
+    console.log('bound!', this.query);
+    this.queryString = `https://www.reddit.com/r/all/search.json?q=${this.query}&limit=5`;
+    console.log('Sandra Sucks', this.queryString);
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.http.get<Post>(this.queryString)
+      .subscribe(result => {
+        this.postList = result.data.children;
+        console.log('this.postList', this.postList);
+        this.postList.forEach(post => {
+          post.data.created = post.data.created * 1000;
+        })
+      });
+  }
+}
+
+export class Post {
+  kind: string;
+  data: PostData;
+}
+
+export class PostData {
+  children: PostChildren[];
+}
+
+export class PostChildren {
+ data: PostChildrenData;
+}
+
+export class PostChildrenData {
+
 }
